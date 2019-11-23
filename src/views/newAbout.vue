fdaeed4c5f64a4f014605960b88b5683676d2dd7
sunshuai <sunshuai2013@foxmail.com>
2019-11-23 16:10:24 +0800

    添加新的组件

---
+++ src/views/newAbout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    这是一个新的组件,从后台获取的数据={{value}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'newAbout',
+  data () {
+    return {
+      value: ''
+    }
+  },
+  created () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      console.log('从后台获取数据开始')
+      this.value = 'aaaa'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
